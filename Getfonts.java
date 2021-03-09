44704f8244e2287d8f9308494ab8eed97bc8d4d1
Nandani1312 <79973699+Nandani1312@users.noreply.github.com>
2021-03-09 18:18:26 +0530

    Create Getfonts.java

---
+++ Getfonts.java
@@ -0,0 +1,10 @@
+
+import java.awt.*;
+class Getfonts{
+    public static void main(String[] args){
+String Allfonts[]=GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+for(int i=0;i<Allfonts.length;i++){
+    System.out.println(Allfonts[i]);
+}
+}
+}
